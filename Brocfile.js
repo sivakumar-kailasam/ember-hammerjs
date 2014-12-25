3da0a021821e80c458b2ca6f9759939e93674fe0
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2014-12-25 21:03:18 +0530

    Added config specific to envs

--- Brocfile.js
+++ Brocfile.js
@@ -18,4 +18,9 @@ var app = new EmberAddon();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import({
+	development: 'bower_components/hammerjs/hammer.js',
+	production: 'bower_components/hammerjs/hammer.min.js'
+});
+
 module.exports = app.toTree();
